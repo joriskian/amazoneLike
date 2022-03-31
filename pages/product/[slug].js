47d262e06de4c8d6307545adcee1c8c1bd6c51e5
joriskian <jo_risk@hotmail.com>
2022-03-31 12:40:41 +0200

    update material-ui icone

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,20 @@
 // [name].js to create a parameter
+import { useRouter } from 'next/router';
 import React from 'react';
+import data from '../../utils/data';
 
 export default function ProductScreen() {
-  return <div></div>;
+  // utilise le router de next
+  const router = useRouter();
+  // recupere le slug
+  const { slug } = router.query;
+  const product = data.products.find((e) => e.slug === slug);
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+  return (
+    <div>
+      <h1> {product.name} </h1>
+    </div>
+  );
 }
